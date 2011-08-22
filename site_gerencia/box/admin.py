0d4fe7764ae2047c1e7e9fd8db49024f5f3ba6a4
Gabriel Reitz Giannattasio <gabrielrg@cianet.ind.br>
2011-08-22 18:43:23 -0300

    Adicionada leitura de status dos setupbox conectados no canal (refs #75)

--- site_gerencia/box/admin.py
+++ site_gerencia/box/admin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding:utf-8 -*-
 
-from models import SetupBox, Pessoa
+from models import SetupBox, Pessoa, PushServer
 from django.contrib import admin
 
 class AdminSetupBox(admin.ModelAdmin):
@@ -12,5 +12,6 @@ class AdminSetupBox(admin.ModelAdmin):
 
 
 admin.site.register(SetupBox, AdminSetupBox)
+admin.site.register(PushServer)
 admin.site.register(Pessoa)
 
--- site_gerencia/box/models.py
+++ site_gerencia/box/models.py
@@ -6,8 +6,6 @@ from fields                   import MACAddressField
 from django.utils.translation import ugettext as _
 
 
-
-
 class SetupBox(models.Model):
     """
     Modelo que define campos, propriedades e comportamento do SetupBox
@@ -21,15 +19,29 @@ class SetupBox(models.Model):
         """
         Exibe texto referente ao ID do status atual
         """
-        if self.status == 0:
-            return "Inoperante"
-        else:
-            return "Positivo e operante"
+        import simplejson
+        import urllib2
+        try:
+            req = urllib2.Request("http://127.0.0.1:8080/channels-stats/?id="+str(self.mac))
+            opener = urllib2.build_opener()
+            f = opener.open(req)
+            json = simplejson.load(f)
+        except: 
+            pass
+        
+        try:
+            if int(json.get('subscribers')) > 0:
+                return _("Online")
+        except: 
+            pass
+        
+        return _("Offline")
+        
     def __unicode__(self):
         return self.mac
     class Meta:
-        verbose_name        = "SetupBox"
-        verbose_name_plural = "SetupBoxes"
+        verbose_name        = _("SetupBox")
+        verbose_name_plural = _("SetupBoxes")
         ordering            = ['-enabled', '-status', 'mac']
 
 
@@ -40,5 +52,20 @@ class Pessoa(models.Model):
     nome = models.CharField(_('Nome'), max_length = 200)
 
 
-
+class PushServer(models.Model):
+    """ Modelo que persiste dados do Http-Push-Stream server """
+    hostname = models.CharField(_('Hostname'), max_length = 255)
+    #time = models.DateTimeField(_('Time'))
+    channels = models.IntegerField(_('Channels'))
+    broadcast_channels = models.IntegerField(_('Broadcast Channels'))
+    published_messages = models.IntegerField(_('Published Messages'))
+    subscribers = models.IntegerField(_('Subscribers'))
+    def __unicode__(self):
+        return u'%s [%d/%d] %d' % (self.hostname, self.channels, self.subscribers, self.published_messages)
+    # TODO: Sobrescrever método de leitura, para que ele leia o JSON e salve na base, 
+    # caso não seja possível, tem que apenas ler o último registro
+    class Meta:
+        verbose_name        = _("Push Server")
+        verbose_name_plural = _("Push Servers")
+        ordering            = ['-subscribers']
 
