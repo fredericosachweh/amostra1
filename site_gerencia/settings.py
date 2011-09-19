199bca33871843131feca643acd380b1b0091618
Gabriel Reitz Giannattasio <gabrielrg@cianet.ind.br>
2011-09-19 16:29:20 -0300

    Settings preparado pra uso no nginx

--- site_gerencia/settings.py
+++ site_gerencia/settings.py
@@ -16,7 +16,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-if 'test' in sys.argv:
+if True: # in sys.argv:
     ## Banco de dados teste
     DATABASES = {
         'default': {
@@ -63,8 +63,8 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-if 'runserver' in sys.argv or 'test' in sys.argv:
-    MEDIA_URL = 'http://127.0.0.1:8000/media/'
+if True: # in sys.argv or 'test' in sys.argv:
+    MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(PROJECT_ROOT_PATH,'media')
     ADMIN_MEDIA_PREFIX = '/static/admin/'
     STATIC_ROOT = os.path.join(PROJECT_ROOT_PATH,'static')
