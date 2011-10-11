505b02f9e1324acac72fd2612937b0f377c70e3c
Gabriel Reitz Giannattasio <gabrielrg@cianet.ind.br>
2011-10-11 18:24:59 -0300

    Adicionado submodulo git contendo django Alterado submodulo git com frontend para pasta raiz Modificadas rotas de url para carregar dist-srv do frontend em /box

--- site_gerencia/box/urls.py
+++ site_gerencia/box/urls.py
@@ -4,10 +4,11 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic.simple import direct_to_template
 from django.conf import settings
+import os
 
 urlpatterns = patterns('',
     url(r'^index/$',        'box.views.index',        name='box_index'),
-    url(r'^$',              'box.views.index'),
+#    url(r'^$',              'box.views.index'),
     url(r'^auth/(?P<mac>[0-9A-F:]{10,20})/.*$','box.views.auth', name='box_auth'),
     
     url(r'^canal_list/$',   'box.views.canal_list',   name='canal_list'),
@@ -17,8 +18,8 @@ urlpatterns = patterns('',
     
     url(r'^ping/$',         'box.views.ping',         name='ping'),
     
-    url(r'^frontend/$',     direct_to_template, {'template': 'frontend/index.html'}),
-    url(r'^frontend/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT+"/frontend/",
+    url(r'^$',     direct_to_template, {'template': 'box/index.html'}),
+    url(r'^(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': os.path.dirname(settings.PROJECT_ROOT_PATH)+"/frontend/dist-srv/",
         }),
     )
