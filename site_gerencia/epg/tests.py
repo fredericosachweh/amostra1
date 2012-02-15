640eac4233069dde024a1733eb11a44feb0e96df
Eduardo Vieira <eduardo@cianet.ind.br>
2012-02-15 20:05:00 -0200

    Implementados testes unitarios para interface REST do EPG. (refs #479 refs #480)

--- site_gerencia/epg/tests.py
+++ site_gerencia/epg/tests.py
@@ -2,6 +2,8 @@
 # -*- encoding:utf-8 -*-
 
 from django.test import TestCase
+from django.test.client import Client
+import simplejson as json
 
 from models import *
 from data_importer import XML_Epg_Importer, Zip_to_XML
@@ -84,8 +86,10 @@ class Test_XML_to_db(object):
 	def test_Programme_1(self):
 		programme = Programme.objects.get(programid='0000257856')
 		self.assertEquals(programme.source, self.epg_source)
-		self.assertItemsEqual(programme.titles.values_list('lang__value', 'value'), [(u'pt', u'BBC Sessions: The Verve'),(u'en', u'BBC Sessions: Verve; The')])
-		self.assertEquals(programme.descriptions.values_list('lang__value','value')[0], (None, u'Uma impressionante atuação do The Verve no famoso estúdio Maida Vale, da BBC. Desfrute desta íntima, porém poderosa gravação da banda de Richard Ashcroft, que inclui músicas como Bitter Sweet Symphony e Love is Noise. - www.revistaeletronica.com.br '))
+		titles = [(u'pt', u'BBC Sessions: The Verve'),(u'en', u'BBC Sessions: Verve; The')]
+		self.assertItemsEqual(programme.titles.values_list('lang__value', 'value'), titles)
+		descs = (None, u'Uma impressionante atuação do The Verve no famoso estúdio Maida Vale, da BBC. Desfrute desta íntima, porém poderosa gravação da banda de Richard Ashcroft, que inclui músicas como Bitter Sweet Symphony e Love is Noise. - www.revistaeletronica.com.br ')
+		self.assertEquals(programme.descriptions.values_list('lang__value','value')[0], descs)
 		self.assertEquals(programme.date, '2008')
 		self.assertItemsEqual(programme.categories.values_list('lang__value', 'value'), [(u'pt',u'Espetáculo'),(u'pt', u'Show')])
 		self.assertEquals(programme.video_colour, u'yes')
@@ -109,11 +113,10 @@ class One_Raw_XML(Test_XML_to_db, TestCase):
 	def tearDown(self):
 		self.f.close()
 		
-	def test_Channels_count(self):
+	def test_Models_count(self):
 		self.assertEquals(Channel.objects.all().count(), 1)
-		
-	def test_Programmes_count(self):
 		self.assertEquals(Programme.objects.all().count(), 1)
+		self.assertEquals(Guide.objects.all().count(), 1)
 		
 class One_Zipped_XML(Test_XML_to_db, TestCase):
 
@@ -136,12 +139,11 @@ class One_Zipped_XML(Test_XML_to_db, TestCase):
 	def tearDown(self):
 		self.f.close()
 		
-	def test_Channels_count(self):
+	def test_Models_count(self):
 		self.assertEquals(Channel.objects.all().count(), 1)
-		
-	def test_Programmes_count(self):
 		self.assertEquals(Programme.objects.all().count(), 1)
-		
+		self.assertEquals(Guide.objects.all().count(), 1)
+
 class Two_Zipped_XMLs(Test_XML_to_db, TestCase):
 
 	def setUp(self):
@@ -154,6 +156,8 @@ class Two_Zipped_XMLs(Test_XML_to_db, TestCase):
 		zipped = ZipFile( self.f, 'w' )
 		zipped.writestr('100.xml', input_xml_1)
 		zipped.writestr('505.xml', input_xml_2)
+		zipped.writestr('101.xml', input_xml_1)
+		zipped.writestr('506.xml', input_xml_2)
 		zipped.close()
 		self.epg_source = Epg_Source(filefield=self.f.name)
 		self.epg_source.save()
@@ -170,14 +174,13 @@ class Two_Zipped_XMLs(Test_XML_to_db, TestCase):
 		self.assertEquals(self.epg_source.generator_info_url, 'http://xmltv.revistaeletronica.com.br')
 		self.assertEquals(self.epg_source.minor_start, parse('20120116000500'))
 		self.assertEquals(self.epg_source.major_stop, parse('20120116034500'))
-		self.assertEquals(self.epg_source.numberofElements, 4)
+		self.assertEquals(self.epg_source.numberofElements, 8)
 	
-	def test_Channels_count(self):
+	def test_Models_count(self):
 		self.assertEquals(Channel.objects.all().count(), 2)
-		
-	def test_Programmes_count(self):
-		self.assertEquals(Programme.objects.all().count(), 2)		
-		
+		self.assertEquals(Programme.objects.all().count(), 2)
+		self.assertEquals(Guide.objects.all().count(), 2)
+
 	def test_Channel_2(self):
 		channel = Channel.objects.get(channelid='505')
 		self.assertEquals(channel.source, self.epg_source)
@@ -188,15 +191,93 @@ class Two_Zipped_XMLs(Test_XML_to_db, TestCase):
 	def test_Programme_2(self):
 		programme = Programme.objects.get(programid='0000025536')
 		self.assertEquals(programme.source, self.epg_source)
-		self.assertItemsEqual(programme.titles.values_list('lang__value', 'value'), [(u'pt', u'Três Homens em Conflito'),(u'en', u'The Good, The Bad and the Ugly')])
-		self.assertEquals(programme.descriptions.values_list('lang__value','value')[0], (None, u'Durante a Guerra Civil Americana, três aventureiros tentam pôr as mãos numa fortuna. - www.revistaeletronica.com.br '))
+		titles = [(u'pt', u'Três Homens em Conflito'),(u'en', u'The Good, The Bad and the Ugly')]
+		self.assertItemsEqual(programme.titles.values_list('lang__value', 'value'), titles)
+		descs = (None, u'Durante a Guerra Civil Americana, três aventureiros tentam pôr as mãos numa fortuna. - www.revistaeletronica.com.br ')
+		self.assertEquals(programme.descriptions.values_list('lang__value','value')[0], descs)
 		self.assertEquals(programme.date, '1966')
 		self.assertItemsEqual(programme.categories.values_list('lang__value', 'value'), [(u'pt',u'Filme'),(u'pt', u'Western')])
 		self.assertEquals(programme.video_colour, u'yes')
 		self.assertEquals(programme.rating.system, u'Advisory')
 		self.assertEquals(programme.rating.value, u'Programa impróprio para menores de 14 anos')
 		self.assertEquals(programme.country.value, u'Itália/Espanha')
-		self.assertItemsEqual(programme.actors.values_list('name'), ( ((u'Clint Eastwood'),), ((u'Lee Van Cleef'),), ((u'Eli Wallach'),), ((u'Rada Rassimov'),), ((u'Mario Brega'),), ))
+		actors = ( ((u'Clint Eastwood'),), ((u'Lee Van Cleef'),), ((u'Eli Wallach'),), ((u'Rada Rassimov'),), ((u'Mario Brega'),), )
+		self.assertItemsEqual(programme.actors.values_list('name'), actors)
 		self.assertItemsEqual(programme.directors.values_list('name'), ( ((u'Sergio Leone'),), ))
 		self.assertItemsEqual(programme.star_ratings.values_list('value'), ( ((u'5/5'),), ))
+		
+	def test_Channel_REST(self):
+		c = Client()
+		response = c.get('/tv/api/channels/')
+		expected = [{'urls': [], 'channelid': '100', 'id': 1, 'display_names': [{'lang': {'value': 'pt'}, 'value': 'Concert Channel'}], \
+		'icons': [{'src': '100.png'}]}, \
+		{'urls': [], 'channelid': '505', 'id': 2, 'display_names': [{'lang': {'value': 'pt'}, 'value': 'Band HD'}], 'icons': [{'src': '505.png'}]}]
+		self.assertItemsEqual(json.loads(response.content), expected)
+	
+	def test_Programme_REST(self):
+		c = Client()
+		response = c.get('/tv/api/programmes/')
+		expected = [{'rating': {'system': 'Advisory', 'value': 'Programa livre para todas as idades'}, \
+		'presenters': [], 'titles': [{'lang': {'value': 'pt'}, 'value': 'BBC Sessions: The Verve'}, {'lang': {'value': 'en'}, 'value': 'BBC Sessions: Verve; The'}], \
+		'guests': [], \
+		'commentators': [], \
+		'id': 1, \
+		'composers': [], \
+		'editors': [], \
+		'writers': [], \
+		'actors': [], \
+		'subtitles': [], \
+		'length': None, \
+		'secondary_titles': [], \
+		'episode_numbers': [], \
+		'descriptions': \
+		[{'value': u'Uma impressionante atuação do The Verve no famoso estúdio Maida Vale, da BBC. Desfrute desta íntima, porém poderosa gravação da banda de Richard Ashcroft, que inclui músicas como Bitter Sweet Symphony e Love is Noise. - www.revistaeletronica.com.br '}], \
+		 'video_aspect': None, \
+		 'date': '2008', \
+		 'categories': [{'lang': {'value': 'pt'}, 'value': u'Espetáculo'}, {'lang': {'value': 'pt'}, 'value': 'Show'}], \
+		 'audio_present': None, \
+		 'video_present': None, \
+		 'producers': [], \
+		 'directors': [], \
+		 'video_colour': 'yes', \
+		 'video_quality': None, \
+		 'adapters': [], \
+		 'audio_stereo': None}, \
+		 {'rating': {'system': 'Advisory', 'value': u'Programa impróprio para menores de 14 anos'}, \
+		 'presenters': [], \
+		 'titles': [{'lang': {'value': 'pt'}, 'value': u'Três Homens em Conflito'}, \
+		 	{'lang': {'value': 'en'}, 'value': 'The Good, The Bad and the Ugly'}], \
+		 'guests': [], \
+		 'commentators': [], \
+		 'id': 2, \
+		 'composers': [], \
+		 'editors': [], \
+		 'writers': [], \
+		 'actors': [{'role': None, 'name': 'Clint Eastwood'}, {'role': None, 'name': 'Lee Van Cleef'}, {'role': None, 'name': 'Eli Wallach'}, \
+		 	{'role': None, 'name': 'Rada Rassimov'}, {'role': None, 'name': 'Mario Brega'}], \
+		 'subtitles': [], \
+		 'length': None, \
+		 'secondary_titles': [], \
+		 'episode_numbers': [], \
+		 'descriptions': [{'value': u'Durante a Guerra Civil Americana, três aventureiros tentam pôr as mãos numa fortuna. - www.revistaeletronica.com.br '}], \
+		 'video_aspect': None, \
+		 'date': '1966', \
+		 'categories': [{'lang': {'value': 'pt'}, 'value': 'Filme'}, {'lang': {'value': 'pt'}, 'value': 'Western'}], \
+		 'audio_present': None, 'video_present': None, \
+		 'producers': [], \
+		 'country': {'value': u'Itália/Espanha'}, \
+		 'directors': [{'name': 'Sergio Leone'}], \
+		 'video_colour': 'yes', \
+		 'video_quality': None, \
+		 'adapters': [], \
+		 'audio_stereo': None}]
+		self.assertItemsEqual(json.loads(response.content), expected)
+		
+	def test_Guide_REST(self):
+		c = Client()
+		response = c.get('/tv/api/guide/')
+		expected = [{'start': '2012-01-16 00:05:00', 'programme_id': 1, 'stop': '2012-01-16 00:45:00', 'channel_id': 1}, \
+		{'start': '2012-01-16 01:45:00', 'programme_id': 2, 'stop': '2012-01-16 03:45:00', 'channel_id': 2}]
+		self.assertItemsEqual(json.loads(response.content), expected)
+		
 		
