cdfb158e5d5eb5b07e9412877225466470b3c7be
Eduardo Vieira <eduardo@cianet.ind.br>
2012-02-17 12:30:18 -0200

    Aceita URI sem a '/' no final. (refs #479)

--- site_gerencia/api/urls.py
+++ site_gerencia/api/urls.py
@@ -20,20 +20,20 @@ programme_filters = programme_filters[:-1] # Remove the last '|'
 urlpatterns = patterns('',
 	
 	# REST interface for channel resources
-	url(r'^channels/$', channel_handler),
-	url(r'^channels/(?P<fields>((%s)(,)*)+)/$' % channel_filters, channel_handler),
-	url(r'^channels/(?P<channel_ids>(\d+(,)*)+)/$', channel_handler),
-	url(r'^channels/(?P<channel_ids>\d+)/(?P<fields>((%s)(,)*)+)/$' % channel_filters, channel_handler),
+	url(r'^channels/?$', channel_handler),
+	url(r'^channels/(?P<fields>((%s)(,)*)+)/?$' % channel_filters, channel_handler),
+	url(r'^channels/(?P<channel_ids>(\d+(,)*)+)/?$', channel_handler),
+	url(r'^channels/(?P<channel_ids>\d+)/(?P<fields>((%s)(,)*)+)/?$' % channel_filters, channel_handler),
 
 	# REST interface for programme resources
-	url(r'^programmes/$', programme_handler),
-	url(r'^programmes/(?P<fields>((%s)(,)*)+)/$' % programme_filters, programme_handler),
-	url(r'^programmes/(?P<programme_ids>(\d+(,)*)+)/$', programme_handler),
-	url(r'^programmes/(?P<programme_ids>\d+)/(?P<fields>((%s)(,)*)+)/$' % programme_filters, programme_handler),
+	url(r'^programmes/?$', programme_handler),
+	url(r'^programmes/(?P<fields>((%s)(,)*)+)/?$' % programme_filters, programme_handler),
+	url(r'^programmes/(?P<programme_ids>(\d+(,)*)+)/?$', programme_handler),
+	url(r'^programmes/(?P<programme_ids>\d+)/(?P<fields>((%s)(,)*)+)/?$' % programme_filters, programme_handler),
 	
 	# REST interface for guide resources
-	url(r'^guide/$', guide_handler),
-	url(r'^guide/(?P<obj>(channels|programmes))/(?P<ids>(\d+(,)*)+)/$', guide_handler),
+	url(r'^guide/?$', guide_handler),
+	url(r'^guide/(?P<obj>(channels|programmes))/(?P<ids>(\d+(,)*)+)/?$', guide_handler),
 	
 	# Auto generated documentation
 	url(r'^$', documentation_view),
