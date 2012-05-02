4a05934217c9c3b2d0292e2fa74028dc811e9095
gartz <gartz@cianet.ind.br>
2012-05-02 18:37:08 -0300

    Corrigidas informações que vão no log

--- site_gerencia/log/views.py
+++ site_gerencia/log/views.py
@@ -2,6 +2,13 @@ from django.http import HttpResponse
 from pprint import pprint
 
 def index(request):
-    pprint(('Log', request.POST, request.META, ))
+    pprint((
+        'Log enviado pelo stdout do Settopbox', 
+        request.META.get('REMOTE_ADDR'), 
+        request.META.get('HTTP_COOKIE'), 
+        request.META.get('HTTP_USER_AGENT'), 
+        request.POST, 
+    ))
+    print('');
     #print(request)
     return HttpResponse('')
