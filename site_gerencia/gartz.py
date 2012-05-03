5b6ac7822609f0f9d031b2c340c61cb3268a1b15
gartz <gartz@cianet.ind.br>
2012-05-03 17:26:37 -0300

    Removido overload de log e tastypie_debug

--- site_gerencia/gartz.py
+++ site_gerencia/gartz.py
@@ -29,44 +29,5 @@ STATIC_URL = '/tvfiles/static/'
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '=rz16epry+8okcm#e=n_m4f4by*-q6-rf^hci!)2yjvadk4lx1'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-
-if DEBUG == True:
-    try:
-        # Debug-Toolbar https://github.com/robhudson/django-debug-toolbar/
-        import debug_toolbar
-        INTERNAL_IPS = ('127.0.0.1',)
-        DEBUG_TOOLBAR_PANELS = (
-            'debug_toolbar.panels.version.VersionDebugPanel',
-            'debug_toolbar.panels.timer.TimerDebugPanel',
-            'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-            'debug_toolbar.panels.headers.HeaderDebugPanel',
-            'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-            'debug_toolbar.panels.template.TemplateDebugPanel',
-            'debug_toolbar.panels.sql.SQLDebugPanel',
-            'debug_toolbar.panels.signals.SignalDebugPanel',
-            'debug_toolbar.panels.logger.LoggingPanel',
-        )
-        INSTALLED_APPS += ('debug_toolbar',)
-        MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-    except ImportError:
-        pass
-
-TASTYPIE_FULL_DEBUG = DEBUG
+INTERNAL_IPS = ('127.0.0.1',)
 
