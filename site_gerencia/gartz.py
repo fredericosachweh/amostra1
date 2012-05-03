250e0e3a21b7a2cd0ccd524412c07d55458330a8
gartz <gartz@cianet.ind.br>
2012-05-03 17:20:00 -0300

    Alterado settings de gartz para extender settings padrao

--- site_gerencia/gartz.py
+++ site_gerencia/gartz.py
@@ -3,23 +3,10 @@
 import sys
 import os
 
-PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-PARENT_PATH =  os.path.dirname(PROJECT_ROOT_PATH)
-
-if PROJECT_ROOT_PATH not in sys.path:
-    sys.path.append(PROJECT_ROOT_PATH)
+from settings import *
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    ('Helber Maciel Guerra', 'helber@cianet.ind.br'),
-    ('Gabriel Reitz Giannattasio', 'gartz@cianet.ind.br'),
-    ('Eduardo Vieira', 'eduardo@cianet.ind.br'),
-	('Claudio Guirunas', 'claudio@cianet.ind.br'),
-)
-
-MANAGERS = ADMINS
 
 DATABASES = {
 	'default': {
@@ -32,37 +19,6 @@ DATABASES = {
 	}
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Sao_Paulo'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'pt-br'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
-
-
-#MEDIA_URL = '/tvfiles/media/'
-#MEDIA_ROOT = '/var/www/html/tvfiles/media/'
-#ADMIN_MEDIA_PREFIX = '/tvfiles/static/admin/'
-#STATIC_ROOT = '/var/www/html/tvfiles/static/'
-#STATIC_URL = '/tvfiles/static/'
-#ROOT_URL = 'tv/'
-
 ROOT_URL = 'tv/'
 MEDIA_URL = '/tvfiles/media/'
 MEDIA_ROOT = os.path.join(PROJECT_ROOT_PATH,'tvfiles','media')
@@ -70,62 +26,8 @@ ADMIN_MEDIA_PREFIX = '/tvfiles/static/admin/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT_PATH,'tvfiles','static')
 STATIC_URL = '/tvfiles/static/'
 
-
-LOGIN_URL = '/'+ROOT_URL+'accounts/login'
-LOGIN_REDIRECT_URL = '/'+ROOT_URL+'administracao/'
-
-#^/canal/(add|remove|edit|delete)/(.*)$
-LOGIN_REQUIRED_URLS = (
-    r'^/%scanal/((?!canallist$))$',
-    r'^/%sadmin/(.*)$',
-)
-
-
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '=rz16epry+8okcm#e=n_m4f4by*-q6-rf^hci!)2yjvadk4lxl'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    #os.path.join(PROJECT_ROOT_PATH,'static/'),
-    #'/var/www/html/%sstatic/',
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'lib.middleware.login.RequireLoginMiddleware',
-)
-
-ROOT_URLCONF = 'urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT_PATH,'templates/')
-)
+SECRET_KEY = '=rz16epry+8okcm#e=n_m4f4by*-q6-rf^hci!)2yjvadk4lx1'
 
 LOGGING = {
     'version': 1,
@@ -144,57 +46,6 @@ LOGGING = {
         },
     }
 }
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-    'django.contrib.markup',
-    # South http://south.aeracode.org/docs/
-    #'south',
-    # Gestao de canal
-    'canal',
-    # Interface dos setup-box
-    'box',
-    # Pagina de home
-    #'home',
-    # Aplicação de controle de stream
-    'stream',
-    'device',
-    # EPG
-    'epg',
-    'tv',
-    'tastypie',
-    # Recebe logs do setopbox, imprime e armazena
-    'log',
-)
-
-
-LOGIN_URL = '/%saccounts/login' % ROOT_URL
-
-LOGIN_REDIRECT_URL = '/%sadministracao/' % ROOT_URL
-
-#^/canal/(add|remove|edit|delete)/(.*)$
-LOGIN_REQUIRED_URLS = (
-    r'^/%scanal/((?!canallist$))$',
-    r'^/%sadmin/(.*)$',
-)
-
-MULTICAST_DAEMON = '/usr/bin/multicat_daemon'
-MULTICAST_COMMAND = '/usr/bin/multicat'
-MULTICAST_APP = 'multicat'
-
-DVBLAST_DAEMON = '/usr/bin/dvblast_daemon'
-DVBLAST_COMMAND = '/usr/bin/dvblast'
-#DVBLAST_COMMAND = '/usr/local/bin/fake_dvblast'
-DVBLAST_APP = 'dvblast'
-DVBLAST_CONF_DIR = '/etc/dvblast'
-
-CHANNEL_RECORD_DIR = '/mnt/backup/gravacoes'
 
 if DEBUG == True:
     try:
@@ -218,8 +69,4 @@ if DEBUG == True:
         pass
 
 TASTYPIE_FULL_DEBUG = DEBUG
-
-
-
-FORCE_SCRIPT_NAME=""
 
