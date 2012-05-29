b76bd5ab645b5aa5d80ed65a18d85b23121293a7
gartz <gartz@cianet.ind.br>
2012-05-29 10:06:57 -0300

    Adicionada ferramenta para retornar IP e fake MAC

--- site_gerencia/tools/views.py
+++ site_gerencia/tools/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.views.decorators.cache import never_cache
 
+@never_cache
 def log(request):
     from pprint import pprint
     pprint((
@@ -27,3 +28,22 @@ def date(request):
     response = '{"timestamp": %s, "timezone": %d}'% (timestamp,timezone)
     return HttpResponse(response)
     #return HttpResponse('2012-05-15 17:14:55.702043')
+    
+@never_cache
+def network(request):
+    import re
+    def DottedIPToInt( dotted_ip ):
+        exp = 3
+        intip = 0
+        for quad in dotted_ip.split('.'):
+                intip = intip + (int(quad) * (256 ** exp))
+                exp = exp - 1
+        return(intip)
+        
+    ip = request.META['REMOTE_ADDR']
+    
+    ipAsInt = DottedIPToInt(ip)
+    formatedMac = re.sub("(.{2})", "\\1:", ('FF%010d' % ipAsInt), re.DOTALL)
+    mac = formatedMac[0:-1]
+    response = '{"mac": "%s", "ip": "%s"}'% (mac,ip)
+    return HttpResponse(response)
