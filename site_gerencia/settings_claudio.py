1a5bbb86685e8bed4ee0ceb8582103dbc9e05df7
Claudio Guirunas <claudio@cianet.ind.br>
2012-06-15 17:35:23 -0300

    Criacao da branch a partir da DEV da versao 0.4.0

--- site_gerencia/settings_claudio.py
+++ site_gerencia/settings_claudio.py
@@ -33,7 +33,6 @@ else:
             'HOST': '127.0.0.1',
             'PORT': '3306'
         }
-
     }
 
 
@@ -67,4 +66,15 @@ ROOT_URL = 'tv/'
 #STATIC_ROOT = os.path.join(PROJECT_ROOT_PATH, 'tvfiles', 'static')
 #STATIC_URL = '/tv/static/'
 
+INTERNAL_IPS = ('127.0.0.1',)
 
+#MessageMiddleware
+#from django.contrib.messages.middleware.MessageMiddleware
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    #'lib.middleware.login.RequireLoginMiddleware',
+)
