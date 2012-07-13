698a8641c3fc113ab7846bf4a86ed017627bf5a2
Helber Maciel Guerra <helber@cianet.ind.br>
2012-07-13 10:36:56 -0300

    Formatações e correção de código.

--- site_gerencia/device/forms.py
+++ site_gerencia/device/forms.py
@@ -3,12 +3,11 @@
 from django import forms
 from django.utils.translation import ugettext as _
 from django.contrib import admin
-from django.core.urlresolvers import reverse
 from django.contrib.admin.widgets import ForeignKeyRawIdWidget
 from django.db.models.fields.related import ManyToOneRel
 from widgets import ContentTypeSelect
-from models import *
-from dvbinfo.models import *
+import models
+from dvbinfo import models as dvbinfo_models
 
 
 class GenericRelationForm(forms.ModelForm):
@@ -17,26 +16,28 @@ class GenericRelationForm(forms.ModelForm):
         try:
             model = self.instance.content_type.model_class()
             model_key = model._meta.pk.name
-        except Exception as ex:
+        except Exception:
             model = self.Meta.model
             model_key = 'id'
         self.fields['object_id'].widget = ForeignKeyRawIdWidget(
             rel=ManyToOneRel(model, model_key), admin_site=admin.site)
         self.fields['content_type'].widget.widget = ContentTypeSelect(
-                        'lookup_id_object_id', 
-                        self.fields['content_type'].widget.widget.attrs, 
+                        'lookup_id_object_id',
+                        self.fields['content_type'].widget.widget.attrs,
                         self.fields['content_type'].widget.widget.choices)
 
 
 class DvbTunerForm(forms.ModelForm):
     class Meta:
-        model = DvbTuner
+        model = models.DvbTuner
         widgets = {
-            'adapter' : forms.Select(),
+            'adapter': forms.Select(),
         }
 
+
 class DvbTunerAutoFillForm(forms.Form):
-    sat = forms.ModelChoiceField(Satellite.objects, label=_(u'Satélite'))
+    sat = forms.ModelChoiceField(dvbinfo_models.Satellite.objects,
+        label=_(u'Satélite'))
     chan = forms.ChoiceField(label=_(u'Canal'))
     fta = forms.BooleanField(label=_(u'Somente canais FTA'), initial=True)
     freq = forms.CharField(label=_(u'Frequência'))
@@ -45,79 +46,79 @@ class DvbTunerAutoFillForm(forms.Form):
     mod = forms.CharField(label=_(u'Modulação'))
     fec = forms.CharField(label=u'FEC')
 
+
 class IsdbTunerForm(forms.ModelForm):
-    class Meta:
-        model = IsdbTuner
-        exclude = ('adapter',)
-    
-#    def clean_free_adapters(self):
-#        fa = int(self.cleaned_data['free_adapters'])
-#        if fa <= 0:
-#            from django.core.exceptions import ValidationError
-#            raise ValidationError(_(u'Deve haver pelo menos 1 adaptador disponível para uso.'))
-#        return self.cleaned_data['free_adapters']
-    
     free_adapters = forms.IntegerField(label=_('Adaptadores livres'))
 
+    class Meta:
+        model = models.IsdbTuner
+        exclude = ('adapter',)
+
+
 class IsdbTunerAutoFillForm(forms.Form):
-    state = forms.ModelChoiceField(State.objects, label=_(u'Estado'))
+    state = forms.ModelChoiceField(dvbinfo_models.State.objects,
+        label=_(u'Estado'))
     city = forms.ChoiceField(label=_(u'Cidade'))
     chan = forms.ChoiceField(label=_(u'Canal'))
     freq = forms.CharField(label=_(u'Frequência'))
 
+
 class UnicastInputForm(forms.ModelForm):
     class Meta:
-        model = UnicastInput
+        model = models.UnicastInput
         widgets = {
-            'interface' : forms.Select(),
-            'protocol' : forms.RadioSelect(),
+            'interface': forms.Select(),
+            'protocol': forms.RadioSelect(),
         }
+
 
 class MulticastInputForm(forms.ModelForm):
     class Meta:
-        model = MulticastInput
+        model = models.MulticastInput
         widgets = {
-            'interface' : forms.Select(),
-            'protocol' : forms.RadioSelect(),
+            'interface': forms.Select(),
+            'protocol': forms.RadioSelect(),
         }
-    
+
     def clean_ip(self):
         from django.core.exceptions import ValidationError
         ip = self.cleaned_data['ip']
         octet = int(ip.split('.')[0])
         if octet < 224 or octet > 239:
-            raise ValidationError(_(u'Endereços multicast devem ter o primeiro octeto entre 224 e 239.'))
+            raise ValidationError(
+                _(u'Endereços multicast devem ter o primeiro octeto \
+entre 224 e 239.'))
         return ip
 
 
 class DemuxedServiceForm(GenericRelationForm):
     class Meta:
-        model = DemuxedService
+        model = models.DemuxedService
 
 
 class FileInputForm(forms.ModelForm):
     class Meta:
-        model = FileInput
+        model = models.FileInput
         widgets = {
-            'filename' : forms.Select(),
+            'filename': forms.Select(),
         }
+
 
 class MulticastOutputForm(GenericRelationForm):
     class Meta:
-        model = MulticastOutput
+        model = models.MulticastOutput
 
 
 class StreamRecorderForm(GenericRelationForm):
     class Meta:
-        model = StreamRecorder
+        model = models.StreamRecorder
 
 
 class UniqueIPForm(GenericRelationForm):
     class Meta:
-        model = UniqueIP
+        model = models.UniqueIP
 
-        
+
 class SoftTranscoderForm(GenericRelationForm):
     class Meta:
-        model = SoftTranscoder
-
+        model = models.SoftTranscoder
