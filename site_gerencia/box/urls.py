76957117abaf437af9129d7a5d71c7fdbd255de2
Helber Maciel Guerra <helber@cianet.ind.br>
2012-07-13 16:45:06 -0300

    Remoção de views não utilizadas

--- site_gerencia/box/urls.py
+++ site_gerencia/box/urls.py
@@ -10,16 +10,12 @@ urlpatterns = patterns('',
     url(r'^index/$',        'box.views.index',        name='box_index'),
     url(r'^auth/(?P<mac>[0-9A-F:]{10,20})/.*$','box.views.auth', name='box_auth'),
     url(r'^programme_info/$',               'box.views.programme_info', name='programme_info'),
-    url(r'^tvod_list/$',                    'box.views.tvod_list', name='tvod_list'),
     url(r'^guide_programmes/$',             'box.views.guide_programmes', name='guide_programmes'),
-    url(r'^get_date_server/$',              'box.views.get_date_server', name='get_date_server'),
     url(r'^guide_mount_line_of_programe/$', 'box.views.guide_mount_line_of_programe', name='guide_mount_line_of_programe'),
     url(r'^channel_programme_info/$',       'box.views.channel_programme_info', name='channel_programme_info'),
     url(r'^remote_log/$',                   'box.views.remote_log',   name='box_remote_log'),
     url(r'^setup/$',                        'box.views.setup',        name='setup'),
-    
     url(r'^ping$',          'box.views.ping',         name='ping'),
-    
     url(r'^debug/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': os.path.dirname(settings.PROJECT_ROOT_PATH)+"/frontend/src/",
         }),
