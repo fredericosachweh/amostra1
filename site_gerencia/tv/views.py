e1862de83900c06912445935c7f68665d92a6c97
Thiago Cechetto <thiagoc@cianet.ind.br>
2012-07-22 21:39:28 -0300

    Função para preencher entradas filtradas pelo tipo

--- site_gerencia/tv/views.py
+++ site_gerencia/tv/views.py
@@ -1,10 +1,11 @@
 # -*- encoding:utf-8 -*-
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
-from django.core.urlresolvers import reverse
+from device.models import *
 
 import models
+import logging
 
 
 def channel_switchlink(request, action, pk):
@@ -17,3 +18,19 @@ def channel_switchlink(request, action, pk):
     if url is None:
         url = reverse('admin:tv_channel_changelist')
     return HttpResponseRedirect(url)
+
+
+def input_list_interfaces(request):
+    log = logging.getLogger('tv.view')
+    pk = request.GET.get('type')
+    log.debug('Type com pk=%s', pk)
+    table = {'arquivos_de_entrada': FileInput.objects.all(),
+             'entradas_multicast': MulticastInput.objects.all(),
+             'entradas_unicast': UnicastInput.objects.all(),
+             'isdb': IsdbTuner.objects.all(),
+             'dvbs': DvbTuner.objects.all()}[pk]
+        
+    response = '<option selected="selected" value="">---------</option>'
+    for v in table:
+        response += ('<option value="%s">%s %s</option>' % (v, v.description, v))
+    return HttpResponse(response)
