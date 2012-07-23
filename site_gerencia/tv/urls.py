efda55b3023a099852a25e066e947d973325dff9
Thiago Cechetto <thiagoc@cianet.ind.br>
2012-07-22 21:38:13 -0300

    Adicao de url para referenciar ajax

--- site_gerencia/tv/urls.py
+++ site_gerencia/tv/urls.py
@@ -10,4 +10,6 @@ urlpatterns = patterns('',
      {'action': 'start'}, 'channel_start'),
     (r'^channel/stop/(?P<pk>\d+)/$', 'tv.views.channel_switchlink',
      {'action': 'stop'}, 'channel_stop'),
+    (r'^input/interfaces/$','tv.views.input_list_interfaces'),
+
 )
