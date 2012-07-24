b0837d861caadf9be0152bfd5057339b37e6c8a6
Diego Lagranha Weiss <diegolw@cianet.ind.br>
2012-07-24 18:05:12 -0300

    Remocao do passo de entrada multiplexada

--- site_gerencia/tv/admin_views.py
+++ site_gerencia/tv/admin_views.py
@@ -1,18 +1,16 @@
 # -*- encoding:utf-8 -*-
-from device.models import UniqueIP, StreamRecorder
+from device.models import UniqueIP, StreamRecorder, FileInput
 from device.models import SoftTranscoder
 from django.contrib.admin.helpers import AdminForm
 from django.contrib.formtools.wizard import FormWizard
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from models import Channel
-from tv.forms import DemuxedServiceFormWizard, InputChooseForm, ChannelForm
+from tv.forms import InputChooseForm, ChannelForm
 from tv.forms import StreamRecorderForm, AudioConfigsForm
 from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 from device.models import DemuxedService
-from django import forms
-from django.utils.translation import ugettext as _
 
 
 class ChannelCreationWizard(FormWizard):
@@ -34,37 +32,17 @@ class ChannelCreationWizard(FormWizard):
             next_form_step = next_form_step + 1
         return next_form_step
 
-    def get_next_form_step(self, request):
-        next_form_step = 2
-        hasDemux = self.has_demux_step(request)
-        if hasDemux:
-            next_form_step = next_form_step + 1
-        return next_form_step
-
     def remove_optional_form(self, model_form):
         if model_form in self.form_list:
             self.form_list.remove(model_form)
 
     def clean_request_session(self, request):
-        # Remove image name from session
         request.session['file_name'] = ''
         request.session.save()
 
     def process_step(self, request, form, step):
-        # Add DemuxedServiceFormWizard
-        if step == 0 and request.POST:
-            hasDemux = self.has_demux_step(request)
-            if hasDemux:
-                if DemuxedServiceFormWizard in self.form_list:
-                    self.form_list.remove(DemuxedServiceFormWizard)
-                self.form_list.insert(1, DemuxedServiceFormWizard)
-            else:
-                if DemuxedServiceFormWizard in self.form_list:
-                    self.form_list.remove(DemuxedServiceFormWizard)
-        # Add optional forms
-        next_form_step = self.get_next_form_step(request)
-        last_step = next_form_step - 1
-        if last_step == step:
+        next_form_step = 2
+        if step == 1:
             if form.cleaned_data.get('audio_config'):
                 next_form_step = self.insert_optional_form(form,
 next_form_step, AudioConfigsForm)
@@ -96,62 +74,24 @@ next_form_step, StreamRecorderForm)
         thumb.thumbnail((200, 200), Image.ANTIALIAS)
         thumb.save(thumb_url)
 
-    def get_logo_name(self, hasDemux):
-        logo_name = '1-image'
-        if hasDemux:
-            logo_name = '2-image'
-        return logo_name
-
-    def get_source_id_name(self, hasDemux):
-        source_id_name = '1-source'
-        if hasDemux:
-            source_id_name = '2-source'
-        return source_id_name
-
-    def get_channel_id(self, hasDemux):
-        channel_id = '1-channelid'
-        if hasDemux:
-            channel_id = '2-channelid'
-        return channel_id
-
-    def get_form_title(self, hasDemux, step, request):
+    def get_form_title(self, step, request):
         title = {None: 'Entrada de Fluxo',
-                 '0': 'Entrada Demultiplexada',
-                 '1': 'Adicionar Canal',
-                 '2': 'Configurar Audio',
-                 '3': 'Configurar Gravação',
-                 '4': None}
-        if not hasDemux:
-            if step >= 0:
-                alter_step = str(int(step) + 1)
-                step = alter_step
-        if step is not None:
-            if ('2-gravar_config' in request.POST or '1-gravar_config' in \
-request.POST):
-                if not ('2-audio_config' in request.POST or '1-audio_config' \
-in request.POST):
-                    step = str(int(step) + 1)
+                 '0': 'Adicionar Canal',
+                 '1': 'Configurar Audio',
+                 '2': 'Configurar Gravação',
+                 '3': None}
         return title[step]
-
-    def has_demux_step(self, request):
-        hasDemux = False
-        if request.POST:
-            input_type = request.POST.get('0-input_types_field')
-            hasDemux = input_type == 'dvbs' or input_type == 'isdb' or \
-input_type == 'entradas_unicast' or input_type == 'entradas_multicast'
-        return hasDemux
 
     def parse_params(self, request, admin=None, *args, **kwargs):
         '''
         Save into request.session the key 'file_name',
         that contains the local where the image was saved.
         '''
-        hasDemux = self.has_demux_step(request)
         if request.FILES:
-            logo_name = self.get_logo_name(hasDemux)
+            logo_name = '1-image'
             file_name = str(request.FILES[logo_name])
             extension = file_name.split('.')[-1]
-            channel_id_key = self.get_channel_id(hasDemux)
+            channel_id_key = '1-number'
             channel_id_value = request.POST[channel_id_key]
             channel_id_as_name = '%s.%s' % (channel_id_value, extension)
             request.session['file_name'] = '%s%s.%s' % (
@@ -162,36 +102,17 @@ self.get_dir_to_save_image(), channel_id_value, extension)
         self._model_admin = admin
         opts = admin.model._meta
         step = request.POST.get('wizard_step')
-        title = self.get_form_title(hasDemux, step, request)
+        title = self.get_form_title(step, request)
         self.extra_context.update({
             'title': title,
             'current_app': admin.admin_site.name,
             'has_change_permission': admin.has_change_permission(request),
             'add': True,
             'opts': opts,
-            #'root_path': admin.admin_site.root_path,
             'app_label': 'opts.app_label',
             'step': step
         })
         return super(ChannelCreationWizard, self).parse_params(request, admin)
-
-    def get_content_type_id(self, content_type_key):
-        ''' Values from device_content_type '''
-        content_type = {'dvbs': 26,
-                        'isdb': 27,
-                        'entradas_unicast': 28,
-                        'entradas_multicast': 29}[content_type_key]
-        return content_type
-
-    def demuxed_input_field(self):
-        objects_ids = []
-        for ip in UniqueIP.objects.all():
-            objects_ids.append(ip.object_id)
-        model = DemuxedService.objects
-        for ob_id in objects_ids:
-            model = model.exclude(deviceserver_ptr_id=ob_id)
-        demuxed_input = forms.ModelChoiceField(model, label=_(u'Entrada'))
-        return demuxed_input
 
     def render_template(self, request, form, previous_fields, step, \
 context=None, step_title=None):
@@ -201,9 +122,11 @@ context=None, step_title=None):
         '''
         if step == 0:
             self.clean_request_session(request)
-        if step == 1:
-            form.fields['demuxed_input'] = self.demuxed_input_field()
-        # Wrap the form into a AdminForm to get the fieldset
+            if AudioConfigsForm in self.form_list:
+                self.remove_optional_form(AudioConfigsForm)
+            if StreamRecorderForm in self.form_list:
+                self.remove_optional_form(StreamRecorderForm)
+
         fieldsets = []
         if form.__class__ == AudioConfigsForm:
             fieldsets = form.fieldsets.__dict__['fieldsets']
@@ -230,11 +153,13 @@ sequential % 256)
         data = {}
         for form in form_list:
             data.update(form.cleaned_data)
-        # Verify and set fields to save
-        hasDemux = self.has_demux_step(request)
-        source_id_name = self.get_source_id_name(hasDemux)
-        # Create UniqueIP connector between DemuxServer n' Channel
-        sink_unique = data['demuxed_input']
+
+        sink_unique = None
+        source_id_name = '1-source'
+        if data['demuxed_input'] and (data['demuxed_input'] != '-1'):
+            sink_unique = DemuxedService.objects.get(id=data['demuxed_input'])
+        else:
+            sink_unique = FileInput.objects.get(id=data['input_stream'])
         uniqueIp = UniqueIP.create(sink_unique)
         sequential = uniqueIp.sequential
         # TODO: Review
