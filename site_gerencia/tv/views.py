697994eae02937df22475bb3da7160398c1ae49d
Diego Lagranha Weiss <diegolw@cianet.ind.br>
2012-07-24 18:06:48 -0300

    Ajax para o demux field

--- site_gerencia/tv/views.py
+++ site_gerencia/tv/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from device.models import FileInput, MulticastInput, UnicastInput, IsdbTuner
 from device.models import DvbTuner
 from django.core.urlresolvers import reverse
+from device.models import UniqueIP, DemuxedService
 
 import models
 import logging
@@ -33,6 +34,34 @@ def input_list_interfaces(request):
              'dvbs': DvbTuner.objects.all()}[pk]
     response = '<option selected="selected" value="">---------</option>'
     for v in table:
-        response += ('<option value="%s">%s %s</option>' % (v, v.description,
-v))
+        response += ('<option value="%s">%s %s</option>' % (v.id,
+v.description, v))
+    return HttpResponse(response)
+
+
+def get_content_type_id(content_type_key):
+    ''' Values from device_content_type '''
+    content_type = {'dvbs': 26,
+                    'isdb': 27,
+                    'entradas_unicast': 28,
+                    'entradas_multicast': 29}[content_type_key]
+    return content_type
+
+
+def get_demux_input_list(request):
+    input_type = request.GET.get('type')
+    if input_type == 'arquivos_de_entrada':
+        f_text = '<option selected="selected" value="-1">Não usado</option>'
+        return HttpResponse(f_text)
+    content_type_id = get_content_type_id(input_type)
+    objects_ids = []
+    for ip in UniqueIP.objects.all():
+        objects_ids.append(ip.object_id)
+    model = DemuxedService.objects.all()
+    for ob_id in objects_ids:
+        model = model.exclude(deviceserver_ptr_id=ob_id)
+    model = model.exclude(content_type_id=content_type_id)
+    response = '<option selected="selected" value="">---------</option>'
+    for m in model:
+        response += ('<option value="%s">%s</option>' % (m.id, m))
     return HttpResponse(response)
