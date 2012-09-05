fedda6c895cf79b8c8256e7e6e1e8b8a2d5ed3a0
Helber Maciel Guerra <helber@cianet.ind.br>
2012-09-05 10:19:54 -0300

    recursos de acesso à referencias do relacionamento tv.Channel e SetTopBox. E exposição do Channel na API do client.

--- site_gerencia/client/api.py
+++ site_gerencia/client/api.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # -*- encoding:utf8 -*-
-
-
-from tastypie.authorization import DjangoAuthorization
+from tastypie.authorization import DjangoAuthorization, Authorization
 from tastypie.api import NamespacedApi
 from tastypie.resources import NamespacedModelResource
 from tastypie import fields
+from tastypie.constants import ALL, ALL_WITH_RELATIONS
+from tastypie.validation import Validation
 import models
+from tv.models import Channel
+
+## Validation:
+#http://stackoverflow.com/questions/7435986/how-do-i-configure-tastypie-to-treat-a-field-as-unique
 
 
 class SetTopBoxResource(NamespacedModelResource):
@@ -17,6 +21,7 @@ class SetTopBoxResource(NamespacedModelResource):
         authorization = DjangoAuthorization()
         allowed_methods = ['get', 'post', 'delete', 'put', 'patch']
         urlconf_namespace = 'client'
+        validation = Validation()
 
 
 class SetTopBoxParameterResource(NamespacedModelResource):
@@ -25,12 +30,47 @@ class SetTopBoxParameterResource(NamespacedModelResource):
 
     class Meta:
         queryset = models.SetTopBoxParameter.objects.all()
-        resource_name = 'parameter'
+        resource_name = 'settopboxparameter'
         authorization = DjangoAuthorization()
         allowed_methods = ['get', 'post', 'delete', 'put', 'patch']
         urlconf_namespace = 'client'
+        always_return_data = True
+        validation = Validation()
+        filtering = {
+            "settopbox": ALL_WITH_RELATIONS,
+            "key": ALL,
+            "value": ALL
+        }
 
+
+class ChannelResource(NamespacedModelResource):
+
+    number = fields.CharField(attribute='number', unique=True)
+
+    class Meta:
+        queryset = Channel.objects.all()
+        resource_name = 'channel'
+        authorization = Authorization()
+
+
+class SetTopBoxChannelResource(NamespacedModelResource):
+
+    settopbox = fields.ForeignKey(SetTopBoxResource, 'settopbox', null=False)
+    channel = fields.ForeignKey(ChannelResource, 'channel', null=False)
+
+    class Meta:
+        queryset = models.SetTopBoxChannel.objects.all()
+        resource_name = 'settopboxchannel'
+        authorization = DjangoAuthorization()
+        always_return_data = True
+        filtering = {
+            "settopbox": ALL,
+            "channel": ALL
+        }
+        validation = Validation()
 
 api = NamespacedApi(api_name='v1', urlconf_namespace='client')
 api.register(SetTopBoxResource())
 api.register(SetTopBoxParameterResource())
+api.register(ChannelResource())
+api.register(SetTopBoxChannelResource())
