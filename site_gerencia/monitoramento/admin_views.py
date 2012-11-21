56aa2f4651777988ce6c5342f7fcd61394220102
Sergio Cioban Filho <cioban@cianet.ind.br>
2012-11-21 16:02:37 -0200

    Retiradas funcoes e comentario que nao eram utilizados

--- site_gerencia/monitoramento/admin_views.py
+++ site_gerencia/monitoramento/admin_views.py
@@ -1,7 +1,5 @@
 # Create your views here.
 
-
-
 import netsnmp
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
@@ -16,7 +14,6 @@ from models import *
 from pprint import pprint
 import sys
 import cgi
-
 
 def _get_snmp_status(server=None):
     if server is None:
@@ -44,25 +41,6 @@ def _get_snmp_status(server=None):
         uptime_h = timedelta(seconds=int(uptime.seconds))
         return [int(uptime.days), str(uptime_h)]
 
-def html_render(items):
-    try:
-        return "<ul><li>"+str(items.next())+'</li>'+html_render(items)+'</ul>'
-    except StopIteration:
-        return ''
-
-def __get_object_str(curr_object):
-    obj_type = str(type(curr_object))
-    obj_type = obj_type.split("'")[1]
-    obj_type = obj_type.split('.').pop()
-    object_representative = eval(obj_type+'_representative')
-    new_object = object_representative(original_obj=curr_object)
-    obj_str = new_object.to_string()
-
-    if obj_str is None:
-        obj_str = str(curr_object)
-
-    return obj_str
-
 def __get_representative_object(curr_object):
     obj_type = str(type(curr_object))
     obj_type = obj_type.split("'")[1]
@@ -88,34 +66,13 @@ def mon_list(request):
     channel_list = []
     for ch in channels:
         if hasattr(ch, 'source'):
-            #next_source = ch.source
-            #aux_list = []
-            #aux_list_html = []
-            #while True:
-
-            #    obj_str = __get_object_str(next_source)
-
-            #    #aux_list.append(cgi.escape('((%s/%s))' %
-            #    #    (next_source, type(next_source))))
-            #    aux_list.append(cgi.escape(obj_str))
-            #    if hasattr(next_source, 'sink'):
-            #        next_source = next_source.sink
-            #    else:
-            #        break
-
-            #aux_list.reverse()
-            #html = html_render(iter(aux_list))
-
             object_r = __get_representative_object(ch.source)
-            #ch_r = MulticastOutput_representative(original_obj=ch.source)
-            #html = ch_r.to_html()
             html = object_r.to_html()
 
             channel_data = {
                 'name': ch.name,
                 'number': ch.number,
                 'html_tree': html,
-                #'sink_list': aux_list,
             }
             channel_list.append(channel_data)
 
