dc9289be2604e8ca3d5a29c12bf0ee0e235155e2
Gabriel Reitz Giannattasio <gartz@cianet.ind.br>
2012-12-04 21:33:37 -0200

    Adicionado mysqldb lib do python

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 yum groupinstall "Development tools" -y
-yum install -y dateutil python-devel libjpeg-turbo libjpeg-turbo-devel openjpeg openjpeg-devel openjpeg-libs turbojpeg turbojpeg-devel gd python-pip tk tk-devel lcms lcms-devel libxml2 libxml2-devel libxslt libxslt-devel
+yum install -y dateutil python-devel libjpeg-turbo libjpeg-turbo-devel openjpeg openjpeg-devel openjpeg-libs turbojpeg turbojpeg-devel gd python-pip tk tk-devel lcms lcms-devel libxml2 libxml2-devel libxslt libxslt-devel mysql-devel MySQL-python
 easy_install simplejson
 easy_install pil
 python-pip install flup
