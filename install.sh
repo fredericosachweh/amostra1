82277933036438aabc18d9e4d249823878326b0c
Gabriel Reitz Giannattasio <gartz@cianet.ind.br>
2012-12-04 21:30:42 -0200

    Adicionada lib lxml

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 yum groupinstall "Development tools" -y
-yum install -y dateutil python-devel libjpeg-turbo libjpeg-turbo-devel openjpeg openjpeg-devel openjpeg-libs turbojpeg turbojpeg-devel gd python-pip tk tk-devel lcms lcms-devel 
+yum install -y dateutil python-devel libjpeg-turbo libjpeg-turbo-devel openjpeg openjpeg-devel openjpeg-libs turbojpeg turbojpeg-devel gd python-pip tk tk-devel lcms lcms-devel libxml2 libxml2-devel libxslt libxslt-devel
 easy_install simplejson
 easy_install pil
 python-pip install flup
+easy_install lxml
