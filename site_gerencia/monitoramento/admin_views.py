328b2135e1b87b733151fb50fe19b1a272e89499
Sergio Cioban Filho <cioban@cianet.ind.br>
2012-12-12 09:36:25 -0200

    Adicionado graficos da arvore de canais

--- site_gerencia/monitoramento/admin_views.py
+++ site_gerencia/monitoramento/admin_views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from datetime import timedelta
+from django.conf import settings
 
 from device.models import Server
 from tv.models import Channel
@@ -58,14 +59,13 @@ def mon_list(request):
         server_dot_cluster[server.name] = pydot.Cluster(
             graph_name=server.name, rankdir="LR",
             label="server: "+server.name, fontsize=20)
-        print dir(server_dot_cluster[server.name])
-        server_data = {}
-        server_data['id']   = server.id
-        server_data['status'] = _get_snmp_status(server.host)
-        server_data['host']   = server.host
-        server_data['name']   = server.name
-        mon_servers.append(server_data)
-        del(server_data)
+        #server_data = {}
+        #server_data['id']   = server.id
+        #server_data['status'] = _get_snmp_status(server.host)
+        #server_data['host']   = server.host
+        #server_data['name']   = server.name
+        #mon_servers.append(server_data)
+        #del(server_data)
 
 
     channels = Channel.objects.all()
@@ -75,33 +75,42 @@ def mon_list(request):
         if hasattr(ch, 'source'):
             object_r = __get_representative_object(ch.source)
             #html = object_r.to_html_tree()
-            html = object_r.to_html_linear()
+            #html = object_r.to_html_linear()
 
             root = object_r.get_root()
             if root not in roots:
                 roots.append(root)
 
-            channel_data = {
-                'name': ch.name,
-                'number': ch.number,
-                'html_tree': html,
-            }
-            channel_list.append(channel_data)
+            #channel_data = {
+            #    'name': ch.name,
+            #    'number': ch.number,
+            #    'html_tree': html,
+            #}
+            #channel_list.append(channel_data)
+
+
+    MEDIA_ROOT = getattr(settings, 'MEDIA_ROOT')
+    GRAPH_ROOT = os.path.join(MEDIA_ROOT, 'mon/roots')
+    if os.path.exists(GRAPH_ROOT) == False:
+        os.makedirs(GRAPH_ROOT)
+
+    root_graph = []
 
     NUM = 1
     for root in roots:
         graph = pydot.Dot(graph_type='digraph', splines='spline')
         #graph.set_node_defaults(shape='circle', fontsize=24)
-        print '=========='
-        print root
         object_r = __get_representative_object(root)
-        #print object_r.to_html_root()
         graph = object_r.to_graph(graph, server_dot_cluster)
         for name, cluster in server_dot_cluster.iteritems():
             graph.add_subgraph(cluster)
-        graph.write_png('graph%d.png' % NUM)
-        graph.write('graph%d.dotfile' % NUM, format='raw', prog='dot')
-        graph.write_svg('graph%d.svg' % NUM)
+        graph_name = 'graph%d.png' % NUM
+        graph_file = os.path.join(GRAPH_ROOT, graph_name)
+        graph.write_png(graph_file)
+        root_graph.append(graph_name)
+        #graph.write('graph%d.dotfile' % NUM, format='raw', prog='dot')
+        #graph.write_svg('graph%d.svg' % NUM)
+
         NUM += 1
 
         del(server_dot_cluster)
@@ -111,11 +120,10 @@ def mon_list(request):
                 graph_name=server.name, rankdir="LR",
                 label="server: "+server.name, fontsize=20)
 
-    html_root = ''
 
     response = render_to_response("admin/mon.html",
         { 'title': 'Monitoramento', 'mon_servers': mon_servers,
-        'channel_list': channel_list, 'html_root': html_root,
+        'channel_list': channel_list, 'root_graph': root_graph,
         }, context_instance=RequestContext(request))
     return response
 
