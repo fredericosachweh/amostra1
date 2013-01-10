ec668e6a6160b78149b4a1b8565191c0c573c9df
root <root@router.gartzdomain>
2013-01-10 16:12:05 -0200

    Arrumei o script de instalação

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 yum groupinstall "Development tools" -y
-yum install -y dateutil python-devel libjpeg-turbo libjpeg-turbo-devel openjpeg openjpeg-devel openjpeg-libs turbojpeg turbojpeg-devel gd python-pip tk tk-devel lcms lcms-devel libxml2 libxml2-devel libxslt libxslt-devel mysql-devel MySQL-python
+yum install -y dateutil python-devel libjpeg-turbo libjpeg-turbo-devel openjpeg openjpeg-devel openjpeg-libs turbojpeg turbojpeg-devel gd python-pip tk tk-devel lcms lcms-devel libxml2 libxml2-devel libxslt libxslt-devel mysql-devel MySQL-python python-imaging
+yum install -y net-snmp net-snmp-python pydot memcached python-memcached
 easy_install simplejson
 easy_install pil
 python-pip install flup
 easy_install lxml
 easy_install django-tastypie
 easy_install pytz
-sudo yum install memcached
-sudo yum install python-memcached
-sudo yum install pydot
-sudo yum install net-snmp
-sudo yum install net-snmp-python
