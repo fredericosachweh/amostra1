516ba7153dac7ec192fd30e44a84ce4984d027ae
Helber Maciel Guerra <helber@cianet.ind.br>
2013-10-10 09:17:46 -0300

    Não utilizado mais

--- runfcgi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-exec /usr/bin/env - \
-  PYTHONPATH="../python:.." \
- ./manage.py runfcgi daemonize=false socket=/tmp/site_gerencia.sock maxrequests=1 debug=true --verbosity=2 --traceback 
