abf7a7bd234806c03fe5a3e87f385e9aa3b4777e
Helber Maciel Guerra <helber@cianet.ind.br>
2013-11-27 15:40:33 -0200

    API tv v2 inicio

--- tv/api.py
+++ tv/api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding:utf8 -*-
 from django.conf import settings
+from django.conf.urls import url
 from tastypie import fields
 from tastypie.authorization import DjangoAuthorization
 from tastypie.authorization import Authorization
@@ -12,7 +13,7 @@ from tastypie.authentication import MultiAuthentication
 from tastypie.authentication import SessionAuthentication
 
 from tastypie.api import NamespacedApi
-from tastypie.resources import NamespacedModelResource
+from tastypie.resources import NamespacedModelResource, ModelResource
 
 from client.models import SetTopBox
 
@@ -48,7 +49,7 @@ class ChannelResource(NamespacedModelResource):
         log.debug('ChannelResource User=%s', user)
         if user.is_anonymous():
             log.debug('Return empt list')
-            return models.Channel.objects.get_empty_query_set()
+            return models.Channel.objects.none()
         object_list = super(ChannelResource, self).apply_authorization_limits(
             request, object_list)
         if user.is_staff:
@@ -78,7 +79,7 @@ class ChannelResource(NamespacedModelResource):
             else:
                 obj_list = models.Channel.objects.all()
         else:
-            obj_list = models.Channel.objects.get_empty_query_set()
+            obj_list = models.Channel.objects.none()
         previous = None
         for o in obj_list:
             o.next = None
@@ -101,5 +102,54 @@ class ChannelResource(NamespacedModelResource):
             bundle.data['next'] = self.get_resource_uri(bundle.obj.next)
         return bundle
 
-api = NamespacedApi(api_name='v1', urlconf_namespace='tv')
-api.register(ChannelResource())
+class AllChannelResource(NamespacedModelResource):
+    #source = fields.CharField(blank=True)
+    next = fields.CharField()
+    previous = fields.CharField()
+
+    class Meta:
+        queryset = models.Channel.objects.filter(enabled=False,
+            source__isnull=False)
+        authorization = ReadOnlyAuthorization()
+        #authorization = SetTopBoxAuthorization()
+        excludes = ['enabled']
+        allowed_methods = ['get']
+        #urlconf_namespace = 'tv'
+        resource_name = 'channel'
+        #api_name = 'v2'
+        fields = ['channelid', 'description', 'name', 'number', 'thumb']
+        authentication = MultiAuthentication(
+            Authentication(),
+            ApiKeyAuthentication())
+
+    def dehydrate(self, bundle):
+        u"This method populate previour and next (linked list)"
+        if not hasattr(bundle.obj, 'previous'):
+            bundle.data['previous'] = None
+        else:
+            bundle.data['previous'] = self.get_resource_uri(
+                bundle.obj.previous)
+        if not hasattr(bundle.obj, 'next'):
+            bundle.data['next'] = None
+        else:
+            bundle.data['next'] = self.get_resource_uri(bundle.obj.next)
+        return bundle
+
+    def obj_get_list(self, bundle, **kwargs):
+        obj_list = super(AllChannelResource, self).obj_get_list(bundle, **kwargs)
+        previous = None
+        for o in obj_list:
+            o.next = None
+            if previous is not None:
+                previous.next = o
+            o.previous = previous
+            previous = o
+        return obj_list
+
+api_v1 = NamespacedApi(api_name='v1', urlconf_namespace='tv_v1')
+api_v1.register(ChannelResource())
+
+api_v2 = NamespacedApi(api_name='v2', urlconf_namespace='tv_v2')
+api_v2.register(AllChannelResource())
+
+apis = [api_v1, api_v2]
