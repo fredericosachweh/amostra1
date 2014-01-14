5f145b605cd10edfb9ad10b3b7df6300998ffa1c
Helber Maciel Guerra <helber@cianet.ind.br>
2014-01-14 15:58:28 -0200

    Filtros no admin

--- client/admin.py
+++ client/admin.py
@@ -1,10 +1,23 @@
 # -*- encoding:utf-8 -*-
 
 
-from django.contrib.admin import site
+from django.contrib.admin import site, ModelAdmin
 import models
+
+
+class SetTopBoxConfigAdmin(ModelAdmin):
+    search_fields = ('settopbox__mac', 'settopbox__serial_number', )
+    list_display = ('settopbox', 'key', 'value', )
+    list_filter = ('settopbox', )
+
+
+class SetTopBoxChannelAdmin(ModelAdmin):
+    search_fields = ('settopbox__mac', 'settopbox__serial_number', )
+    list_display = ('settopbox', 'channel', )
+    list_filter = ('settopbox', )
+
 
 site.register(models.SetTopBox)
 site.register(models.SetTopBoxParameter)
-site.register(models.SetTopBoxChannel)
-site.register(models.SetTopBoxConfig)
+site.register(models.SetTopBoxChannel, SetTopBoxChannelAdmin)
+site.register(models.SetTopBoxConfig, SetTopBoxConfigAdmin)
