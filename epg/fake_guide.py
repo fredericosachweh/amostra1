fc52da48c30598d4fe4f6955d0489d77f55b372b
Douglas Figueiredo <eng.douglasfigueiredo@gmail.com>
2014-07-21 11:44:10 -0300

    improve done

--- epg/fake_guide.py
+++ epg/fake_guide.py
@@ -14,12 +14,12 @@ def programmeXML(start, stop, channel, rate):
     programme = etree.Element('programme')
     programme.attrib['start'] = start
     programme.attrib['stop'] = stop
-    programme.attrib['channel'] = channel
+    programme.attrib['channel'] = channel.decode("utf-8")
 
     # create XML - Title
     title_pt = etree.Element('title')
     title_pt.attrib['lang'] = 'pt'
-    title_pt.text = channel
+    title_pt.text = u'Programação ' + channel.decode("utf-8")
     programme.append(title_pt)
 
     # create XML - Credits
@@ -66,51 +66,61 @@ def main(argv):
     time_zone = '-0300'
     current_date = datetime.now()
     programme_interval = 1
-    guide_days = 14
+    guide_days = 60
     guide_offset = (guide_days / 2) * 24
     initial_time = current_date - timedelta(hours=guide_offset)
     final_time = current_date + timedelta(hours=guide_offset)
     rate = '0'
-    channel = 'CIANET CHANNEL'
+    channel = []
+    channel.append('CIANET CHANNEL')
 
     try:
-        opts, args = getopt.getopt(argv, "hc:i:r:")
+        opts, args = getopt.getopt(argv, "hc:i:r:f:")
     except getopt.GetoptError:
-        print 'fake_guide.py -c <nome do canal> -i <intervalo de cada programa> -r <rating do programa>'
+        print 'fake_guide.py -c <lista de canais separados por , > -i <intervalo de cada programa> -r <rating do programa> or -f <lista de canais em arquivo>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'fake_guide.py -c <nome do canal> -i <intervalo de cada programa> -r <rating do programa>'
+            print 'fake_guide.py -c <lista de canais separados por , > -i <intervalo de cada programa> -r <rating do programa> or -f <lista de canais em arquivo>'
             sys.exit()
         elif opt in ("-c"):
-            channel = arg
+            channel = arg.split(",")
+        elif opt in ("-f"):
+            try:
+                channel = []
+                fp = open(arg, 'r')
+                for line in fp:
+                    channel.append(line.replace('\n', '').replace('\r', ''))
+            except ValueError:
+                print 'arquivo invalido'
+                sys.exit()
         elif opt in ("-i"):
             try:
                 programme_interval = int(arg)
             except ValueError:
-                print 'interval deve ser um numero'
+                print 'intervalio deve ser um numero'
                 sys.exit()
         elif opt in ("-r"):
             rate = arg
-
     # create XML - TV
     tv = etree.Element('tv')
     tv.attrib['generator-info-name'] = 'CIANET'
     tv.attrib['generator-info-url'] = 'www.cianet.ind.br'
-
-    while (initial_time < final_time):
-        start = initial_time
-        stop = initial_time + timedelta(hours=programme_interval)
-        initial_time = stop
-        tv.append(programmeXML(start.strftime("%Y%m%d%H%M%S") + ' ' + time_zone,
-                               stop.strftime("%Y%m%d%H%M%S") + ' ' + time_zone,
-                               channel, rate))
+    for c in channel:
+        initial_aux = initial_time
+        while (initial_aux < final_time):
+            start = initial_aux
+            stop = initial_aux + timedelta(hours=programme_interval)
+            initial_aux = stop
+            tv.append(programmeXML(start.strftime("%Y%m%d%H%M%S") + ' ' + time_zone,
+                                   stop.strftime("%Y%m%d%H%M%S") + ' ' + time_zone,
+                                   c, rate))
 
     # pretty string
     xml_guide = etree.tostring(tv, pretty_print=True, xml_declaration=True, encoding='iso-8859-1')
-    
+
     # Save to XML file
-    fakexml = open('fakexml.xml', 'w')
+    fakexml = open('fake_guide.xml', 'w')
     fakexml.write(xml_guide)
 
 if __name__ == "__main__":
