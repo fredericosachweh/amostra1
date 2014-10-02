cda22e3c9e9ef79f3d1e0ba72cdf118f16a68211
Helber Maciel Guerra <helber@cianet.ind.br>
2014-10-02 16:51:03 -0300

    Problema na importação

--- client/__init__.py
+++ client/__init__.py
@@ -1,12 +1,12 @@
 # -*- encoding:utf8 -*
-from . import dispatch
-from dbsettings.utils import set_defaults
-from client import models as clientmodels
-
-set_defaults(clientmodels,
-    ('SetTopBox', 'auto_create', False),
-    ('SetTopBox', 'auto_add_channel', False),
-    ('SetTopBox', 'auto_enable_recorder_access', False),
-    ('SetTopBox', 'use_mac_as_serial', True),
-)
-
+#from . import dispatch
+#from dbsettings.utils import set_defaults
+#from client import models as clientmodels
+#
+#set_defaults(clientmodels,
+#    ('SetTopBox', 'auto_create', False),
+#    ('SetTopBox', 'auto_add_channel', False),
+#    ('SetTopBox', 'auto_enable_recorder_access', False),
+#    ('SetTopBox', 'use_mac_as_serial', True),
+#)
+#
