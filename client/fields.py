36ab31750b052993cbdcb776d77e693bbad35659
Helber Maciel Guerra <helber@cianet.ind.br>
2014-10-09 13:32:04 -0300

    Campo com problema na migração

--- client/fields.py
+++ client/fields.py
@@ -3,14 +3,16 @@ from __future__ import unicode_literals, absolute_import
 
 import re
 
+
 from django.db import models
 from .forms import MACAddressFormField
 
 MAC_RE = r'^([0-9a-fA-F]{2}([:-]?|$)){6}$'
 mac_re = re.compile(MAC_RE)
 
-class MACAddressField(models.Field):
+class MACAddressField(models.CharField):
     empty_strings_allowed = False
+
     def __init__(self, *args, **kwargs):
         kwargs['max_length'] = 17
         super(MACAddressField, self).__init__(*args, **kwargs)
@@ -22,3 +24,6 @@ class MACAddressField(models.Field):
         defaults = {'form_class': MACAddressFormField}
         defaults.update(kwargs)
         return super(MACAddressField, self).formfield(**defaults)
+
+from south.modelsinspector import add_introspection_rules
+add_introspection_rules([], ["^client\.fields\.MACAddressField"])
