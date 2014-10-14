fd95132af454dee59af06809746feecb14b7b10b
Helber Maciel Guerra <helber@cianet.ind.br>
2014-10-14 12:00:54 -0300

    Desabilitando nose por hora

--- iptv/settings.py
+++ iptv/settings.py
@@ -138,7 +138,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates"
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT_PATH, 'templates'), 
+    os.path.join(PROJECT_ROOT_PATH, 'templates'),
 )
 
 IPTV_LOG_DIR = '/iptv/var/log'
@@ -309,7 +309,7 @@ INSTALLED_APPS = (
     # South http://south.aeracode.org/docs/
     # 'south',
     # Nose tests
-    'django_nose',
+    # 'django_nose',
     # Aplicação de controle de stream
     'device',
     # EPG
