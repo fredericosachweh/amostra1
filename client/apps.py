8fe7e30fe2c1252f108bf7a0c02666ff2e73ba38
Helber Maciel Guerra <helber@cianet.ind.br>
2014-10-31 11:33:57 -0200

    Movendo para app django 1.7

--- client/apps.py
+++ client/apps.py
@@ -8,3 +8,15 @@ from django.utils.translation import ugettext_lazy as _
 class ClientConfig(AppConfig):
     name = 'client'
     verbose_name = _('Client')
+
+    def ready(self):
+        from . import dispatch
+        from dbsettings.utils import set_defaults
+        from client import models as clientmodels
+
+        set_defaults(clientmodels,
+           ('SetTopBox', 'auto_create', False),
+           ('SetTopBox', 'auto_add_channel', False),
+           ('SetTopBox', 'auto_enable_recorder_access', False),
+           ('SetTopBox', 'use_mac_as_serial', True),
+        )
