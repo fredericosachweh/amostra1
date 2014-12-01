97140264dbf27e2a92739b4766491356a41d584b
Helber Maciel Guerra <helber@cianet.ind.br>
2014-12-01 17:15:03 -0200

    Legibilidade de código

--- tv/urls.py
+++ tv/urls.py
@@ -3,13 +3,19 @@
 
 from django.conf.urls import patterns
 
-urlpatterns = patterns('',
-    # Channel
-    (r'^channel/start/(?P<pk>\d+)/$', 'tv.views.channel_switchlink',
-     {'action': 'start'}, 'channel_start'),
-    (r'^channel/stop/(?P<pk>\d+)/$', 'tv.views.channel_switchlink',
-     {'action': 'stop'}, 'channel_stop'),
-    (r'^input/interfaces/$', 'tv.views.input_list_interfaces'),
-    (r'^input/demux/$', 'tv.views.get_demux_input_list'),
-
+urlpatterns = patterns(
+    '',
+    (
+        r'^channel/start/(?P<pk>\d+)/$', 'tv.views.channel_switchlink',
+        {'action': 'start'}, 'channel_start'
+    ),
+    (
+        r'^channel/stop/(?P<pk>\d+)/$', 'tv.views.channel_switchlink',
+        {'action': 'stop'}, 'channel_stop'),
+    (
+        r'^input/interfaces/$', 'tv.views.input_list_interfaces'
+    ),
+    (
+        r'^input/demux/$', 'tv.views.get_demux_input_list'
+    ),
 )
