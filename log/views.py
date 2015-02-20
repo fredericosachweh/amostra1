4dff35b074d29f4053a6f28a8d1317c4ec20b104
frederico.matos <frederico.matos@cianet.ind.br>
2015-02-20 16:30:39 -0200

    Ajuste data de exibição das tasks e retira log do gitignore.

--- log/views.py
+++ log/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.core import serializers
 from django.core.serializers.python import Serializer
 from django.http import HttpResponse
+from django.utils import timezone
 
 from . import models
 
@@ -18,7 +19,7 @@ def current_task_log(request):
             current_task = { 
                 'name': log.get_name_display(),
                 'create_at': datetime.datetime.strftime(
-                log.create_at, '%d/%m/%Y %H:%I:%S'),
+                timezone.localtime(log.create_at), '%d/%m/%Y %H:%I:%S'),
                 'progress': "{}%".format(log.progress)
             }
             current_list.append(current_task)
