139520910d716ee87363c07cf730262db58b578b
frederico.matos <frederico.matos@cianet.ind.br>
2015-03-17 15:22:40 -0300

    Pega lista correta de pks dos stbs pras tasks.

--- client/tasks.py
+++ client/tasks.py
@@ -21,15 +21,15 @@ def split_pks(pks):
 @task(name='reload-channels')
 def reload_channels(settopboxes_pks, message, task_id, channel=False):
     splitted_pks = split_pks(settopboxes_pks)
-    nstbs = len(splitted_pks)
+    nstbs = len(settopboxes_pks)
     log.debug('Reload queue to %s STBs', nstbs)
     task = TaskLog.objects.get(pk=task_id)
     for n, pks in enumerate(splitted_pks):
-        settopboxes = SetTopBox.objects.filter(pk__in=settopboxes_pks)
+        settopboxes = SetTopBox.objects.filter(pk__in=pks)
         for settopbox in settopboxes:
             log.debug('Reload:%s', settopbox)
             settopbox.reload_channels(message=message, channel=False)
-        task.progress = D(n) / nstbs * 100
+        task.progress = D(n) / len(splitted_pks) * 100
         task.save()
         if nstbs > 10:
             time.sleep(settings.TASK_INTERVAL)
@@ -42,25 +42,30 @@ def reload_channels(settopboxes_pks, message, task_id, channel=False):
 @task(name='reload-frontend-stbs')
 def reload_frontend_stbs(settopboxes_pks, task_id):
     splitted_pks = split_pks(settopboxes_pks)
+    nstbs = len(settopboxes_pks)
+    log.debug('Reload frontend to %s STBs', nstbs)
     task = TaskLog.objects.get(pk=task_id)
     for n, pks in enumerate(splitted_pks):
-        settopboxes = SetTopBox.objects.filter(pk__in=settopboxes_pks)
+        settopboxes = SetTopBox.objects.filter(pk__in=pks)
         for settopbox in settopboxes:
             settopbox.reload_frontend_stb()
         task.progress = D(n) / len(splitted_pks) * 100
         task.save()
         time.sleep(settings.TASK_INTERVAL)
     task.is_finished = True
+    task.progress = 100
     task.save()
+    log.debug('Reload frontend queue Finished:%s', task)
 
 
 @task(name='reboot-stbs')
 def reboot_stbs(settopboxes_pks, task_id):
     splitted_pks = split_pks(settopboxes_pks)
+    nstbs = len(settopboxes_pks)
+    log.debug('Reboot %s STBs', nstbs)
     task = TaskLog.objects.get(pk=task_id)
     for n, pks in enumerate(splitted_pks):
         settopboxes = SetTopBox.objects.filter(pk__in=pks)
-        log.debug('Reboot')
         nbs = Nbridge.objects.filter(status=True)
         log.debug('NBS=%s', nbs)
         for nbridge in nbs:
@@ -92,25 +97,35 @@ def reboot_stbs(settopboxes_pks, task_id):
             task.save()
             time.sleep(settings.TASK_INTERVAL)
     task.is_finished = True
+    task.progress = 100
     task.save()
+    log.debug('Reboot stbs queue Finished:%s', task)
 
 
 @task(name='accept-recorder')
 def accept_recorder(settopboxes_pks, message, task_id, channel=False):
     task = TaskLog.objects.get(pk=task_id)
+    nstbs = len(settopboxes_pks)
+    log.debug('Accept recorder to %s STBs', nstbs)
     settopboxes = SetTopBoxChannel.objects.filter(
         settopbox__id__in=settopboxes_pks, recorder=False)
     settopboxes.update(recorder=True)
     task.is_finished = True
+    task.progress = 100
     task.save()
+    log.debug('Accept recorder queue Finished:%s', task)
 
 
 @task(name='refuse-recorder')
 def refuse_recorder(settopboxes_pks, message, task_id, channel=False):
     task = TaskLog.objects.get(pk=task_id)
+    nstbs = len(settopboxes_pks)
+    log.debug('Refuse recorder to %s STBs', nstbs)
     settopboxes = SetTopBoxChannel.objects.filter(
         settopbox__id__in=settopboxes_pks, recorder=True)
     settopboxes.update(recorder=False)
     task.is_finished = True
+    task.progress = 100
     task.save()
+    log.debug('Refuse recorder queue Finished:%s', task)
 
