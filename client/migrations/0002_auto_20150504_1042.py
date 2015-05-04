422b15adbc6ac1723c864910e4f1961bd1ea3e12
Frederico Matos <frederico@turbosys.com.br>
2015-05-04 10:43:01 -0300

    Unificando migrations

--- client/migrations/0002_auto_20150504_1042.py
+++ client/migrations/0002_auto_20150504_1042.py
@@ -2,13 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('tv', '0001_initial'),
-        ('client', '0002_settopbox_parent'),
+        ('client', '0001_initial'),
     ]
 
     operations = [
@@ -20,6 +21,8 @@ class Migration(migrations.Migration):
                 ('slug', models.SlugField()),
                 ('is_active', models.BooleanField(default=True, verbose_name='Ativo')),
                 ('order', models.IntegerField(null=True, blank=True)),
+                ('value', models.DecimalField(default=0.0, verbose_name='Valor', max_digits=10, decimal_places=2)),
+                ('tvod_value', models.DecimalField(default=0.0, verbose_name='TVoD Valor', max_digits=10, decimal_places=2)),
             ],
             options={
                 'ordering': ('order',),
@@ -32,10 +35,12 @@ class Migration(migrations.Migration):
             name='PlanChannel',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('channel', models.ForeignKey(to='tv.Channel', unique=True)),
+                ('channel', models.ForeignKey(verbose_name='Canal', to='tv.Channel', unique=True)),
                 ('plan', models.ForeignKey(to='client.Plan')),
             ],
             options={
+                'verbose_name': 'Canal do plano',
+                'verbose_name_plural': 'Canais do plano',
             },
             bases=(models.Model,),
         ),
@@ -43,6 +48,12 @@ class Migration(migrations.Migration):
             model_name='plan',
             name='channels',
             field=models.ManyToManyField(to='tv.Channel', verbose_name='Canais', through='client.PlanChannel'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='settopbox',
+            name='parent',
+            field=models.ForeignKey(related_name='parent_set', on_delete=django.db.models.deletion.SET_NULL, verbose_name='SetTopBox Principal', blank=True, to='client.SetTopBox', null=True),
             preserve_default=True,
         ),
         migrations.AddField(
